420e9b0369971399480887e53f9d107b5472c637
cloud-official <tixu.ramp@gmail.com>
2025-07-12 03:06:11 -0400

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,14 @@
+{
+  "name": "roulette-hub-bot",
+  "version": "1.0.0",
+  "description": "Roulette Hub Discord Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.11.0",
+    "express": "^4.18.2",
+    "ms": "^2.1.3"
+  }
+}
